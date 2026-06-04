7aa041f9f8dca3531fb3b6565b71194d5796f00b
Murtaza Siddiqui <murtazasiddiqui250@gmail.com>
2026-06-05 03:04:33 +0500

    Add files via upload

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Link, useLocation } from "react-router";
+import { app, auth, db } from '../../../src/lib/firebase';
+
 import "./navbar.css"
+
+import { getAuth, signOut } from 'firebase/auth';
+
+const handleLogout = async () => {
+    try {
+        await signOut(auth);
+        console.log('User logged out');
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 const Navbar = () => {
 
@@ -9,12 +22,12 @@ const Navbar = () => {
     return (
         <nav>
             <ul className='navBar'>
-                
+
                 <li> <Link to={'/'}> Home </Link> </li>
-                <li> <Link to={'/about'}> about </Link> </li>
+                <li> <Link to={'/about'}> About </Link> </li>
                 <li> <Link to={'/contact'}> Contact </Link> </li>
                 <li> <Link to={'/services'}> Services </Link> </li>
-                <li> <Link to={'/projects'}> Projects </Link> </li>                
+                <li> <Link to={'/projects'}> Projects </Link> </li>
                 <li><Link to={'/product'}>Product</Link></li>
                 <li> <Link to={'/counter'}> Counter </Link> </li>
                 <li> <Link to={'/todo'}> Todo </Link> </li>
@@ -22,10 +35,12 @@ const Navbar = () => {
                 <li> <Link to={'/users'}> Users </Link> </li>
                 <li> <Link to={'/animation'}> Animation </Link> </li>
                 <li> <Link to={'/love'}> For Her </Link> </li>
-                <li> <Link to={'/login'}> Login </Link> </li>
-                <li> <Link to={'/signup'}> SignUp </Link> </li>
-                
+                {/* <li> <Link to={'/login'}> Login </Link> </li>
+                <li> <Link to={'/signup'}> SignUp </Link> </li> */}
             </ul>
+            <button onClick={handleLogout}>
+                Logout
+            </button>
         </nav>
     )
 }
