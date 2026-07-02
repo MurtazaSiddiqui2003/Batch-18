2cd6f6bb3bc75e932055fd40ca6f748f07efecfa
Murtaza Siddiqui <murtazasiddiqui250@gmail.com>
2026-07-03 03:04:50 +0500

    Add files via upload

--- pages/projects/css-projects/css.jsx
+++ pages/projects/css-projects/css.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import mycssProjects from './myCss.js'
+import './css.css'
 
 const CssProjects = () => {
+  // Pass the data directly into useState to avoid the infinite loop
+  const [projects] = useState(mycssProjects.projects)
+
+  console.log(projects)
+
   return (
-    <div>CssProjects</div>
+    <div className='jsProjects'>
+      <h1>My CSS Projects</h1>
+      {/* <div className="projects">
+        {projects.map((e) => {
+          return (
+            // Added the 'key' prop here using e.id
+            <div className='projectCard' key={e.id}>
+              <img src={e.img} alt={e.name} className='projectImage' />
+              <h3>{e.name}</h3>
+              <a href={`https://${e.link}`} target="_blank" rel="noreferrer">
+                View Project
+              </a>
+            </div>
+          )
+        })}
+      </div> */}
+    </div>
   )
 }
 
